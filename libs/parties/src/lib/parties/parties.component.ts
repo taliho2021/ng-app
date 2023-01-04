98b52d05c22fbe0b9f21c0898e89959761ca59e2
taliho2021 <tal@analink.com>
2023-01-04 16:40:29 -0500

    Added backend server

---
+++ libs/parties/src/lib/parties/parties.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ng-app-parties',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './parties.component.html',
+  styleUrls: ['./parties.component.scss'],
+})
+export class PartiesComponent {}
