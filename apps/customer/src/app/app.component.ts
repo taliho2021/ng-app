c610d04fd836097d5f5938cdcda8eedc14f82c2b
taliho2021 <tal@analink.com>
2023-01-12 16:32:17 -0500

    Removed app.module.ts to make module0less appl

--- apps/customer/src/app/app.component.ts
+++ apps/customer/src/app/app.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ng-app-root',
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
--- apps/customer/src/main.ts
+++ apps/customer/src/main.ts
@@ -1,7 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {importProvidersFrom} from '@angular/core';
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./app/app.routes";
 
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers:[
+        importProvidersFrom(RouterModule.forRoot(appRoutes))
+    ]
+
+})
+  .catch(err => console.error(err));
