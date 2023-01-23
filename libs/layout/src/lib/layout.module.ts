e804488a9c46c6f05f10559efe5ac583a4a51444
taliho2021 <tal@analink.com>
2023-01-23 16:48:35 -0500

    updated sidenav parties component

--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -3,9 +3,14 @@ import { LayoutComponent } from './containers/layout/layout.component';
 import { MaterialModule } from '@ng-app/material';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule, RouterModule],
+  imports: [CommonModule, MaterialModule, RouterModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, LayoutModule],
   declarations: [LayoutComponent],
   exports : [LayoutComponent]
 })
