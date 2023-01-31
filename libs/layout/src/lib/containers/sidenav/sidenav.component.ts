e1f7c80b1a999740875ca11fe5dd26c779320572
Tal Yo <tal@analink.com>
2023-01-31 08:47:10 -0500

    Updated sidenav component

--- libs/layout/src/lib/containers/sidenav/sidenav.component.ts
+++ libs/layout/src/lib/containers/sidenav/sidenav.component.ts
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { Router } from '@angular/router';
 import { SideBarMenuItem } from './sidebar.menu.model';
 import { SidebarNavService } from './sidebar-nav.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'ng-app-sidenav',
@@ -14,29 +23,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./sidenav.component.scss'],
   animations: [
     trigger('indicatorRotate', [
-      state('collapsed', style({transform: 'rotate(0deg)'})),
-      state('expanded', style({transform: 'rotate(180deg)'})),
-      transition('expanded <=> collapsed',
+      state('collapsed', style({ transform: 'rotate(0deg)' })),
+      state('expanded', style({ transform: 'rotate(180deg)' })),
+      transition(
+        'expanded <=> collapsed',
         animate('225ms cubic-bezier(0.4,0.0,0.2,1)')
       ),
-    ])
-  ]
+    ]),
+  ],
 })
-export class SidenavComponent implements OnInit{
-
-   expanded!: boolean;
+export class SidenavComponent implements OnInit {
+  expanded!: boolean;
   @HostBinding('attr.aria-expanded') ariaExpanded = this.expanded;
-  @Input()  item!: SideBarMenuItem;
+  @Input() item!: SideBarMenuItem;
   @Input() depth!: number;
 
-  constructor( public sidenav: SidebarNavService,
-               public router: Router) {
-                if (this.depth === undefined) {
-                  this.depth = 0;
-                }
-               }
+  constructor(public sidenav: SidebarNavService, public router: Router) {
+    if (this.depth === undefined) {
+      this.depth = 0;
+    }
+  }
 
-  ngOnInit()  {
+  ngOnInit() {
     this.sidenav.currentUrl.subscribe((url: string) => {
       if (this.item.route && url) {
         // console.log(`Checking '/${this.item.route}' against '${url}'`);
@@ -47,7 +55,7 @@ export class SidenavComponent implements OnInit{
     });
   }
 
-  onItemSelected(item:SideBarMenuItem) {
+  onItemSelected(item: SideBarMenuItem) {
     if (!this.item.children || !this.item.children.length) {
       this.router.navigate([this.item.route]);
       this.sidenav.closeNav();
@@ -56,4 +64,4 @@ export class SidenavComponent implements OnInit{
       this.expanded = !this.expanded;
     }
   }
-  }
+}
